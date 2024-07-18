67e4af3c1aff7979159dd22b7f14bc2b4148c914
Thispara <paradon.take@gmail.com>
2024-07-18 11:36:27 +0700

    sdfsd

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
-  origin: 'https://react-mysql-front.vercel.app', // Update with your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.urlencoded({ extended: true }));
